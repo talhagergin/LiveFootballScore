e4c276ec838a2827b4e8393b4638f3328555be84
Talha Gergin <talhagergin6@gmail.com>
2024-12-15 16:51:01 +0300

    Initial Commit

---
+++ LiveFootballScore/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LiveFootballScore
+//
+//  Created by Talha Gergin on 15.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LiveFootballScore/LiveFootballScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveFootballScoreApp.swift
+//  LiveFootballScore
+//
+//  Created by Talha Gergin on 15.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct LiveFootballScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

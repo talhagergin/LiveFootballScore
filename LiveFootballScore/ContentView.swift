17b108c9551c2dfc41deb78b32b05df444c9b8a4
Talha Gergin <talhagergin6@gmail.com>
2025-01-05 18:28:37 +0300

    TabBar control is adjusted.

--- LiveFootballScore/ContentView.swift
+++ LiveFootballScore/ContentView.swift
@@ -10,32 +10,29 @@ import SwiftUI
 struct ContentView: View {
     @State private var selectionItem = 0
     var body: some View {
-        /*
-        TabView(selection: $selectionItem){
-            LeaguesView(selectionTabItem: $selectionItem)
-                .tabItem{
-                    Label("Ligler", systemImage: "sportscourt")
-                        .environment(\.symbolVariants, selectionItem == 0 ? .fill : .none)
-                }
-                .tag(0)
-            
-            ProfileView(selectionTabItem: $selectionItem)
-                .tabItem{
-                    Label("Hesabım",systemImage: "person")
-                        .environment(\.symbolVariants, selectionItem == 0 ? .fill : .none)
-                }
-                .tag(1)
-        }*/
-        
+        TabView(selection: $selectionItem) {
             NavigationView {
-                LeaguesView()
+                LeaguesView(selectionTabItem: $selectionItem)
             }
-        
+            .tabItem {
+                Label("Ligler", systemImage: "sportscourt")
+                    .environment(\.symbolVariants, selectionItem == 0 ? .fill : .none)
+            }
+            .tag(0)
+
+            NavigationView {
+                ProfileView(selectionTabItem: $selectionItem)
+            }
+            .tabItem {
+                Label("Hesabım", systemImage: "person")
+                    .environment(\.symbolVariants, selectionItem == 1 ? .fill : .none)
+            }
+            .tag(1)
         }
     }
-    
-
+}
 
 #Preview {
     ContentView()
 }
+    
